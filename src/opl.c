b4660040efb988cf0d35ef289cdb5997807b396a
KrahJohlito <KrahJohlito@outlook.com>
2019-09-27 23:43:16 +0930

    gui: improve notifications code, slightly

--- src/opl.c
+++ src/opl.c
@@ -129,10 +129,7 @@ void moduleUpdateMenu(int mode, int themeChanged, int langChanged)
 
     if (langChanged) {
         guiUpdateScreenScale();
-        if (lngGetGuiValue() != 0) {
-            showLngPopup = 1;
-            popupSfxPlayed = 0;
-        }
+        guiCheckNotifications(0, langChanged);
     }
 
     // refresh Hints
@@ -160,10 +157,7 @@ void moduleUpdateMenu(int mode, int themeChanged, int langChanged)
     // refresh Cache
     if (themeChanged) {
         submenuRebuildCache(mod->subMenu);
-        if (thmGetGuiValue() != 0) {
-            showThmPopup = 1;
-            popupSfxPlayed = 0;
-        }
+        guiCheckNotifications(themeChanged, 0);
     }
 }
 
